12730984dae251674632dde0b64181a555dbf764
programarivm <info@programarivm.com>
2021-01-20 15:48:07 +0100

    Loaded the category fixtures

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -8,13 +8,34 @@ use Doctrine\Persistence\ObjectManager;
 
 class CategoryFixtures extends Fixture
 {
+    private $sample = [
+        [
+            'eCommerce',
+            'ecommerce',
+            'Lorem ipsum dolor sit amet',
+        ],
+        [
+            'Entertainment',
+            'entertainment',
+            'Consectetur adipiscing elit',
+        ],
+        [
+            'Wiki',
+            'wiki',
+            'sed do eiusmod tempor incididunt',
+        ],
+    ];
+
     public function load(ObjectManager $manager)
     {
-        $category = new Category();
-        $category->setName('eCommerce');
-        $category->setSlug('ecommerce');
-        $category->setDescription('This is the description.');
-        $manager->persist($category);
+        for ($i = 0; $i < count($this->sample); $i++) {
+            $category = (new Category())
+                            ->setName($this->sample[$i][0])
+                            ->setSlug($this->sample[$i][1])
+                            ->setDescription($this->sample[$i][2]);
+            $manager->persist($category);
+            $this->addReference("category-$i", $category);
+        }
 
         $manager->flush();
     }
