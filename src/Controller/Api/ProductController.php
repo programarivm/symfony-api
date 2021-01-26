c701f1314ed7b9b111efa3154ab6605299dab947
programarivm <info@programarivm.com>
2021-01-26 17:36:47 +0100

    Replaced product annotations with custom routes

--- src/Controller/Api/ProductController.php
+++ src/Controller/Api/ProductController.php
@@ -5,24 +5,14 @@ namespace App\Controller\Api;
 use App\Entity\Category;
 use App\Entity\Product;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
-use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-/**
- * @Route("/product")
- */
 class ProductController extends AbstractFOSRestController
 {
-    /**
-     * @Rest\Get("/all")
-     *
-     * @return Response
-     */
     public function all(SerializerInterface $serializer): Response
     {
         $repo = $this->getDoctrine()->getRepository(Product::class);
@@ -33,11 +23,6 @@ class ProductController extends AbstractFOSRestController
         );
     }
 
-    /**
-     * @Rest\Post("/create")
-     *
-     * @return Response
-     */
     public function create(Request $request): Response
     {
         $data = json_decode($request->getContent());
@@ -66,11 +51,6 @@ class ProductController extends AbstractFOSRestController
         return new Response(Response::HTTP_CREATED);
     }
 
-    /**
-     * @Rest\Get("/featured/{iso}")
-     *
-     * @return Response
-     */
     public function featured(
         HttpClientInterface $httpClient,
         SerializerInterface $serializer,
