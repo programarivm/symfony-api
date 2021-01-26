7558b26b38f962f4eec53aa76d26a7a1c4162212
programarivm <info@programarivm.com>
2021-01-26 17:28:24 +0100

    Replaced category annotations with custom routes

--- src/Controller/Api/CategoryController.php
+++ src/Controller/Api/CategoryController.php
@@ -4,23 +4,13 @@ namespace App\Controller\Api;
 
 use App\Entity\Category;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
-use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
-/**
- * @Route("/category")
- */
 class CategoryController extends AbstractFOSRestController
 {
-    /**
-     * @Rest\Get("/all")
-     *
-     * @return Response
-     */
     public function all(SerializerInterface $serializer): Response
     {
         $repo = $this->getDoctrine()->getRepository(Category::class);
@@ -31,11 +21,6 @@ class CategoryController extends AbstractFOSRestController
         );
     }
 
-    /**
-     * @Rest\Post("/create")
-     *
-     * @return Response
-     */
     public function create(Request $request): Response
     {
         $data = json_decode($request->getContent());
@@ -54,11 +39,6 @@ class CategoryController extends AbstractFOSRestController
         return new Response(Response::HTTP_CREATED);
     }
 
-    /**
-     * @Rest\Delete("/delete/{id}")
-     *
-     * @return Response
-     */
     public function delete(int $id): Response
     {
         $repo = $this->getDoctrine()->getRepository(Category::class);
@@ -75,11 +55,6 @@ class CategoryController extends AbstractFOSRestController
         return new Response(Response::HTTP_NO_CONTENT);
     }
 
-    /**
-     * @Rest\Put("/update/{id}")
-     *
-     * @return Response
-     */
     public function update(int $id, Request $request): Response
     {
         $data = json_decode($request->getContent());
